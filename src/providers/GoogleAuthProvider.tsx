762ea7bcc53b4708672d02c2a1f8150c38b4aade
intellectro <oluebubeaneke247@gmail.com>
2026-03-10 11:48:27 -0700

    feat(google-auth): added the google auth key

--- src/providers/GoogleAuthProvider.tsx
+++ src/providers/GoogleAuthProvider.tsx
@@ -1,13 +1,12 @@
 import { DefaultType } from "@/models/types/shared/project-type";
-// import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const GoogleAuthProvider = ({ children }: DefaultType) => {
-    // return(
-    //     <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}`}>
-    //         {children}
-    //     </GoogleOAuthProvider>
-    // );
-    return children;
+    return (
+        <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}`}>
+            {children}
+        </GoogleOAuthProvider>
+    );
 }
 
 export default GoogleAuthProvider;
